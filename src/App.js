a5b7f8f7e352d8d7aebd3e7019a7b286008747a7
fgalimzh <galimzhanov.faizulla@gmail.com>
2024-08-13 15:49:26 +0500

    hello 9

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useEffect } from "react";
 import { useTelegram } from "./hooks/useTelegram";
-import {Header} from "./components/Header/Header";
+import Header from "./components/Header/Header";
 import { Route, Routes } from "react-router-dom";
 import RegisterForm from "./components/RegisterForm/RegisterForm";
 import Menu from "./components/Menu/Menu";

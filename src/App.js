6063ca700b7866a36b022adf8fb688418be71bdf
fgalimzh <galimzhanov.faizulla@gmail.com>
2024-08-13 15:45:07 +0500

    hello 8

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
+import { useTelegram } from "./hooks/useTelegram";
+import {Header} from "./components/Header/Header";
+import { Route, Routes } from "react-router-dom";
+import RegisterForm from "./components/RegisterForm/RegisterForm";
+import Menu from "./components/Menu/Menu";
 
 function App() {
+  const { tg } = useTelegram();
+
+  useEffect(() => {
+    tg.ready();
+  }, []);
+
   return (
     <div className="App">
-      hi
+      hello there
+      <Header />
+      <Routes>
+        <Route index element={<Menu />} />
+        <Route path={"regform"} element={<RegisterForm />} />
+      </Routes>
     </div>
   );
 }

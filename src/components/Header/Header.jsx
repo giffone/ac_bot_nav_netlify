23db73f83238549607ab2e6889de27989851b7d0
fgalimzh <galimzhanov.faizulla@gmail.com>
2024-08-14 15:41:52 +0500

    hello 4

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,6 +8,7 @@ const Header = () => {
 
   return (
     <div className={"header"}>
+      ver 2
       <Button onClick={onClose}>Close</Button>
       <span className={"username"}>{user?.username}</span>
     </div>

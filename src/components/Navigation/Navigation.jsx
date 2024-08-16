040c9d493643f67b115b6f27f7d0d20246f20b5f
fgalimzh <galimzhanov.faizulla@gmail.com>
2024-08-16 16:41:06 +0500

    hello 4 8

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,25 +2,25 @@ import React from "react";
 import "./Navigation.css";
 import { useTelegram } from "../../hooks/useTelegram";
 
-const formTypeAbout = "/about";
-const formTypeRegistration = "/regform";
+// const formTypeAbout = "/about";
+// const formTypeRegistration = "/regform";
 
 const Navigation = () => {
   const { tg } = useTelegram();
 
   const onClickAbout = () => {
-    const data = {
-      form_type: formTypeAbout,
-    };
-    tg.sendData(JSON.stringify(data));
+    // const data = {
+    //   form_type: formTypeAbout,
+    // };
+    // tg.sendData(JSON.stringify(data));
     tg.close();
   };
 
   const onClickRegistration = () => {
-    const data = {
-      form_type: formTypeAbout,
-    };
-    tg.sendData(JSON.stringify(data));
+    // const data = {
+    //   form_type: formTypeRegistration,
+    // };
+    // tg.sendData(JSON.stringify(data));
     tg.close();
   };
 
